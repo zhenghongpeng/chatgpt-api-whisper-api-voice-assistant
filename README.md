f0b7ae151b45b7056694f4b464c02bcdb2540b61
zhenghongpeng <zhenghongpeng@yahoo.com>
2023-03-21 17:18:02 -0500

    init

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # chatgpt-api-whisper-api-voice-assistant
-chatgpt api and whisper api tutorial - voice conversation with therapist
+chatgpt api and whisper api - voice conversation with chatgpt as therapist
 
-Video Demo / Tutorial:
-
-https://www.youtube.com/watch?v=Si0vFx_dJ5Y
 
 ## Setup
 
-Install `ffmpeg` with:
+Install `ffmpeg` with for Mac:
 ```
 brew install ffmpeg
+```
+
+Setup python env <your env> using pyenv-virtualenv or conda:
+```
+pyenv viertualenv <your env>
+pyenv activate <your env>
+pip install -r requirements.txt
+```
+
+## Cmd to run the app:
+
+copy .env.example to .env file and add OPENAI_API_KEY:
+
+```
+python therapist.py
 ```
--- therapist.py
+++ therapist.py
@@ -1,18 +1,33 @@
+from time import sleep
+
 import gradio as gr
-import openai, config, subprocess
-openai.api_key = config.OPENAI_API_KEY
+import openai, subprocess, os
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+
+# Obtain the Environment Variables from .env file
+dotenv_path = Path(".env")
+load_dotenv(dotenv_path=dotenv_path)
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 messages = [{"role": "system", "content": 'You are a therapist. Respond to all input in 25 words or less.'}]
 
 def transcribe(audio):
     global messages
 
-    audio_file = open(audio, "rb")
+    os.rename(audio, audio+".wav")
+    audio_file = open(audio+".wav", "rb")
+    # print(audio_file)
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    # print(f"user transcription: {transcript}")
 
     messages.append({"role": "user", "content": transcript["text"]})
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    # print(f"chatGPT response: {response}")
 
     system_message = response["choices"][0]["message"]
     messages.append(system_message)
